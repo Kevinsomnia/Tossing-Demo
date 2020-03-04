5c25821384c1c38a345b1fcfe8712a20f08ce5ca
Kevin Tong <kevinsomnia17@gmail.com>
2020-03-03 21:21:37 -0500

    Basic tossing done.

--- Assets/Main Project/Scripts/TossController.cs
+++ Assets/Main Project/Scripts/TossController.cs
@@ -1,72 +1,117 @@
 ﻿using UnityEngine;
 
 public class TossController : MonoBehaviour {
-    public float throwForce = 1f;
+    public float maxGrabDistance = 5f;          // Can grab objects within this distance.
+    public float dragForce = 25f;               // Higher values = snappier drag.
+    public float dragDistance = 1f;             // Target distance the object should be from the camera when dragging.
+    public float tossForce = 4f;                // Release toss force.
+    public float dragTensionLimit = 200f;       // Minimum threshold before auto-releasing object due to something blocking it's path.
+    public Transform handRig;
+    public GameObject openHand;
+    public GameObject closedHand;
 
     private Transform cachedTrans;
     private Camera cachedCam;
     private Rigidbody currentlyHolding;
-    private float holdDist;
+    private float prevHeldVelocity;
+    private float rigidOrigAngDrag;
 
     private void Awake() {
         cachedTrans = transform;
         cachedCam = GetComponent<Camera>();
         currentlyHolding = null;
+        prevHeldVelocity = 0f;
     }
-
+    
     private void Update() {
+        UpdateHandVisual();
+
         if(Input.GetMouseButtonDown(0)) {
             // Start dragging object on left click.
             Ray rayToCursor = cachedCam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(rayToCursor, out hit, 50f)) {
+            if(Physics.Raycast(rayToCursor, out hit, maxGrabDistance)) {
                 if(currentlyHolding != null) {
                     // Ensure previously held object is released.
-                    ReleaseAndToss();
+                    ReleaseHeldObject(false);
                 }
 
-                currentlyHolding = hit.rigidbody;
-
-                if(currentlyHolding != null) {
-                    // This is a physics object and not just a static wall.
-                    holdDist = Vector3.Distance(cachedTrans.position, currentlyHolding.position);
-                    Debug.Log("Holding " + currentlyHolding.name + " at distance from camera: " + holdDist);
-                }
+                HoldObject(hit.rigidbody);
             }
         }
         else {
-            if(currentlyHolding != null) {
-                if(Input.GetMouseButtonUp(0)) {
-                    // Released left mouse button.
-                    ReleaseAndToss();
-                }
-                else if(Input.GetMouseButtonDown(1)) {
-                    // Throw away from camera upon right click.
-                    Vector3 dirToObject = (currentlyHolding.position - cachedTrans.position).normalized;
-                    currentlyHolding.AddForce(dirToObject * throwForce, ForceMode.Impulse);
-                    currentlyHolding.AddTorque(Random.insideUnitSphere * 0.001f * throwForce, ForceMode.Impulse);
-                    ReleaseAndToss();
-                }
+            if(Input.GetMouseButtonUp(0) && currentlyHolding != null) {
+                // Released left mouse button.
+                ReleaseHeldObject(true);
+            }
+        }
+    }
+    
+    private void FixedUpdate() {
+        if(currentlyHolding != null) {
+            // Get the target position of dragged object.
+            Vector3 targetPoint = GetPointTowardsCursor(dragDistance);
+
+            // Check for abrupt deceleration in velocity while dragging.
+            // Usually means there is something blocking it's path (if high enough).
+            float decelAmount = prevHeldVelocity - currentlyHolding.velocity.magnitude;
+
+            if(prevHeldVelocity - currentlyHolding.velocity.magnitude > dragTensionLimit) {
+                ReleaseHeldObject(true);
+            }
+            else {
+                // Instantly update velocity towards target point.
+                currentlyHolding.velocity = (targetPoint - currentlyHolding.position) * dragForce;
+                prevHeldVelocity = currentlyHolding.velocity.magnitude;
             }
         }
     }
 
-    private void FixedUpdate() {
-        // Handle dragging in FixedUpdate.
-        if(currentlyHolding != null) {
-            currentlyHolding.position = cachedTrans.position + (GetDirectionTowardsMouseCursor() * holdDist);
-            currentlyHolding.rotation = cachedTrans.rotation;
+    private void HoldObject(Rigidbody rigid) {
+        if(rigid == null) {
+            ReleaseHeldObject(false);
+            return;
         }
+
+        currentlyHolding = rigid;
+        prevHeldVelocity = currentlyHolding.velocity.magnitude;
+
+        // "Freeze" rotation while held. Restore angular drag upon releasing.
+        rigidOrigAngDrag = currentlyHolding.angularDrag;
+        currentlyHolding.angularDrag = 10f;
     }
 
-    private void ReleaseAndToss() {
-        //currentlyHolding.AddForce(Vector3.up, ForceMode.Impulse);
+    private void ReleaseHeldObject(bool dampenVelocity) {
+        if(currentlyHolding == null)
+            return;
+        
+        if(dampenVelocity) {
+            // Dampen velocity before releasing, since dragging the object relies on a lot of force.
+            currentlyHolding.velocity *= (tossForce / dragForce);
+        }
+
+        // Restore angular drag and add some random torque. More torque when released with more force.
+        currentlyHolding.angularDrag = rigidOrigAngDrag;
+        float torqueAmount = 0.0005f + (currentlyHolding.velocity.magnitude * 0.002f);
+        currentlyHolding.AddTorque(Random.onUnitSphere * torqueAmount, ForceMode.Impulse);
         currentlyHolding = null;
     }
     
-    private Vector3 GetDirectionTowardsMouseCursor() {
+    private Vector3 GetPointTowardsCursor(float distance) {
         Ray rayToCursor = cachedCam.ScreenPointToRay(Input.mousePosition);
-        return rayToCursor.direction;
+        return cachedTrans.position + (rayToCursor.direction * distance);
+    }
+
+    private void UpdateHandVisual() {
+        // Position hand where drag distance is.
+        Vector3 handPos = GetPointTowardsCursor(dragDistance - 0.1f);
+        Quaternion handRot = Quaternion.LookRotation(handPos - cachedTrans.position);
+        handRig.SetPositionAndRotation(handPos, handRot);
+
+        // Update finger.
+        bool showClosedHand = (currentlyHolding != null);
+        closedHand.SetActive(showClosedHand);
+        openHand.SetActive(!showClosedHand);
     }
 }
---
+++ Assets/Main Project/Shaders/Hand.shader
@@ -0,0 +1,51 @@
+﻿Shader "FX/Hand Shader" {
+	Properties {
+		_Color ("Empty Color", Color) = (0.5, 0.5, 0.5, 1)
+	}
+
+	SubShader {
+		Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+		Blend SrcAlpha One
+		Cull Off
+		ZWrite Off
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+			
+			fixed4 _Color;
+			sampler2D _CameraDepthTexture;
+
+			struct appdata_t {
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+			};
+			
+			struct v2f {
+				float4 vertex : POSITION;
+				half fresnel : TEXCOORD3;
+			};
+			
+			v2f vert(appdata_t v) {
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);				
+				half3 viewDir = normalize(ObjSpaceViewDir(v.vertex));
+				o.fresnel = (1.0 - abs(dot(v.normal, viewDir)));
+				return o;
+			}
+
+			fixed4 frag(v2f i) : COLOR {
+				fixed4 color = _Color;
+				color.rgb = lerp(color.rgb, 1.0, i.fresnel * i.fresnel); // White rim lighting.
+				color.a *= i.fresnel;
+				return color;
+			}
+			ENDCG
+		}
+	}
+
+	Fallback Off
+}
